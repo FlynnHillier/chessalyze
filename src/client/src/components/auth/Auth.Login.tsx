a8ae1af0ff1a2631bee231b6af410313ca80c13e
FlynnHillier <flynnhillier@gmail.com>
2022-12-02 16:40:20 +0000

    updated AuthLogin to utilise Form component; updated useLogin;

--- src/client/src/components/auth/Auth.Login.tsx
+++ src/client/src/components/auth/Auth.Login.tsx
@@ -1,40 +1,53 @@
 import React,{useState} from 'react'
 import { useLogin } from '../../hooks/useLogin'
+import Form from "../util/Form"
 
 const AuthLogin = () => {
-    const {error,isLoading,login,} = useLogin()
+    const {errorMessage,isLoading,clearErrorMessage,login,} = useLogin()
 
     const [username,setUsername] = useState<string>("")
     const [password,setPassword] = useState<string>("")
     
-
-
-
     async function onSubmit(e:React.FormEvent) {
         e.preventDefault()
         login(username,password)
     }
-    
-    
+
     return (
-        <form
-        onSubmit={onSubmit}
-        >
-        <h1>
-            login
-        </h1>
-        <label>
-            username:
-        </label>
-        <input value={username} onInput={(e)=>{setUsername((e.target as HTMLInputElement).value)}}></input>
-        <label>
-            password:
-        </label>
-        <input value={password} onInput={(e)=>{setPassword((e.target as HTMLInputElement).value)}}></input>
-    
-        <text> {error ? `error: ${error.message ? error.message : "an unexpected error occured"}` : ""} </text>
-        <button disabled={isLoading}> Submit </button>
-        </form>
+        <Form
+            title={"login"}
+            isLoading={isLoading}
+            onSubmit={()=>{
+                login(username,password)
+            }}
+            errorState={{
+                value:errorMessage,
+                clear:clearErrorMessage
+            }}
+            submitButtonText={"signup"}
+            fields={[
+                {
+                    label:"username",
+                    placeholder:"username",
+                    type:"text",
+                    state:{
+                        set:setUsername,
+                        value:username
+                    },
+                    required:true
+                },
+                {
+                    label:"password",
+                    placeholder:"password",
+                    type:"password",
+                    state:{
+                        set:setPassword,
+                        value:password
+                    },
+                    required:true
+                }
+            ]}
+        />
     )
 }
 
