5e752508f9cad792dc68030b21201b89578ae23a
FlynnHillier <flynnhillier@gmail.com>
2022-12-02 16:10:34 +0000

    added GameProvider to app

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -9,19 +9,23 @@ import ChessInterface from './views/ChessInterface';
 import AuthLogin from './components/auth/Auth.Login';
 import AuthSignup from './components/auth/Auth.Signup';
 import AuthLogout from './components/auth/Auth.logout';
-
+import GameProvider from './contexts/game.context';
+import TempLoadGame from './components/TempLoadGame';
 
 function App() {
   return (
     <div className="App">
       <SocketProvider>
         <AuthProvider>
-          <ChessInterface/>
-          <AuthLogin/>
-          <Name/>
-          <Status/>
-          <AuthSignup/>
-          <AuthLogout/>
+          <GameProvider>
+            <ChessInterface/>
+            <AuthLogin/>
+            <Name/>
+            <Status/>
+            <AuthSignup/>
+            <AuthLogout/>
+            <TempLoadGame/>
+          </GameProvider>
         </AuthProvider>
       </SocketProvider>
     </div>
