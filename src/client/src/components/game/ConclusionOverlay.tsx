1bc155b3a4c9b3e369bfe9623dcab4a1ee823180
FlynnHillier <flynnhillier@gmail.com>
2022-12-25 18:56:39 +0000

    design changes of conclusion pop up;

--- src/client/src/components/game/ConclusionOverlay.tsx
+++ src/client/src/components/game/ConclusionOverlay.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import "./../../styles/gameOverOverlay.css"
 import {GameConclusion} from "../../types/chessboard"
 import FancyButton from '../util/FancyButton'
+import { Color } from 'chess.js'
 
 interface Props {
     conclusionState:GameConclusion | null
@@ -10,36 +11,55 @@ interface Props {
     width:number
 }
 
-const ConclusionOverlay = (props:Props) => {  
+const ConclusionOverlay = ({conclusionState,isHidden,hideSelf,width}:Props) => {  
+    let [messageText,setMessageText] = useState<string>("")
+    
+    function getDisplayText() : string {
+        if(!conclusionState){
+            return "invalid conclusion."
+        }
+
+        let string = ""
+        if(conclusionState.type === "w"){
+            string = "white wins"
+        }
+        if(conclusionState.type === "b"){
+            string = "black wins"
+        }
+        if(conclusionState.type === "draw"){
+            string = "draw"
+        }
+
+        return `${string} by ${conclusionState.reason}`
+    }
+
+    useEffect(()=>{
+        setMessageText(getDisplayText())
+    },[conclusionState])
+
     return (
-        <div className="chessboard-overlay" style={props.isHidden ? {display:"none"} : {}}
+        <div className="chessboard-overlay" style={isHidden ? {display:"none"} : {}}
             onClick={(e)=>{
                 if(e.target == e.currentTarget){
-                    props.hideSelf()
+                    hideSelf()
                 }
             }}
         >
-            <div className="chessboard-game-over-popup"
-                // style={{
-                //     width:props.width,
-                // }}
-            >
-                {props.conclusionState === null ? 
+            <div className="chessboard-game-over-popup">
+                {conclusionState === null ? 
                     <> This game has not yet <br/> concluded.</> : 
                     <>
                         <div className="chessboard-game-over-header"> 
                             <h2>GAME OVER!</h2>
                         </div>
                         <div>
-                            <text>{props.conclusionState.type} {props.conclusionState.type !== "draw" ? "wins" : ""}</text>
-                            <br/>
-                            <text>by {props.conclusionState.reason}</text>
+                            {messageText}
                         </div>
                         <div>
                             <FancyButton
                                 isLoading={false}
                                 text={"close"}
-                                onClick={props.hideSelf}
+                                onClick={hideSelf}
                             />
                         </div>
                     </>
--- src/client/src/types/chessboard.ts
+++ src/client/src/types/chessboard.ts
@@ -1,5 +1,5 @@
 export type GameConclusionReason = "insufficient material" | "3-move repitition" | "50-move rule" | "checkmate" | "stalemate"
-export type GameConclusionType = "white" | "black" | "draw"
+export type GameConclusionType = "w" | "b" | "draw"
 
 export interface GameConclusion {
     type:GameConclusionType
