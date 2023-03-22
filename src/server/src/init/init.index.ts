a0e7f2b2b625d7d87b79e626fd9133299b3c3e4c
FlynnHillier <flynnhillier@gmail.com>
2023-03-22 18:26:05 +0000

    improved init function; no longer uncaugh promise exception on invalid mongo auth;

--- src/server/src/init/init.index.ts
+++ src/server/src/init/init.index.ts
@@ -11,8 +11,14 @@ import {router} from "../routes/router"
 import socketRouter from "../sockets/index.socket"
 
 
-export function init() {
-    establishMongoConnection(process.env.MONGO_ACCESS_URI as string)
-    app.use(router)
+export async function init(silent = false) {
+    await establishMongoConnection(process.env.MONGO_ACCESS_URI as string)
+    if(!silent)
+    console.log("established connection to mongo DB")
+
     socketRouter(io)
+    if(!silent)
+    console.log("socket server up")
+
+    app.use(router)
 }
