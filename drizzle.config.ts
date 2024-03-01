8a6c6c4eee85fbdc036dd1f4fa0bf671e8d86c1a
flynnhillier <flynnhillier@gmail.com>
2024-03-01 20:40:34 +0000

    drizzle.config.ts

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit"
+
+import { env } from "~/env"
+
+export default defineConfig({
+  schema: "./src/lib/drizzle/schema.ts",
+  out: "./drizzle",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: env.POSTGRES_URL,
+  },
+  verbose: true,
+  strict: true,
+})
