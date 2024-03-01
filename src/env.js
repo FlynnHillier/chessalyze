a636f11730028fad27fd0c010d328d5b6dd5a48f
flynnhillier <flynnhillier@gmail.com>
2024-03-01 17:50:58 +0000

    env DATABASE_URL -> POSTGRES_URL

--- src/env.js
+++ src/env.js
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z
+    POSTGRES_URL: z
       .string()
       .refine(
         (str) => !str.includes("YOUR_POSTGRES_URL_HERE"),
