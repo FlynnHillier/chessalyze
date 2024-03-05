2994f4597f8f587e54ef871f7e0290833632afa9
flynnhillier <flynnhillier@gmail.com>
2024-03-05 19:19:14 +0000

    lobby provider

--- src/app/(dynamic)/(protected)/layout.tsx
+++ src/app/(dynamic)/(protected)/layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 import { authOptions } from "~/server/auth";
 import { redirect } from "next/navigation";
 import { GameProvider } from "~/app/_components/providers/game.provider";
+import { LobbyProvider } from "~/app/_components/providers/lobby.provider";
 
 export default async function RestrictedLayout({ children }: { children: ReactNode }) {
   const session = await getServerSession(authOptions)
@@ -13,9 +14,11 @@ export default async function RestrictedLayout({ children }: { children: ReactNo
 
   return (
     <>
-      <GameProvider>
-        {children}
-      </GameProvider>
+      <LobbyProvider>
+        <GameProvider>
+          {children}
+        </GameProvider>
+      </LobbyProvider>
     </>
   )
 }
--- src/app/(dynamic)/(protected)/play/page.tsx
+++ src/app/(dynamic)/(protected)/play/page.tsx
@@ -1,13 +1,17 @@
 "use client"
 
 import { useGame } from "~/app/_components/providers/game.provider"
+import { useLobby } from "~/app/_components/providers/lobby.provider"
+
 
 export default function GamePage() {
   const { game } = useGame()
+  const { lobby } = useLobby()
 
   return (
     <>
-      {`${game.present}`}
+      {`in game: ${game.present}`} <br />
+      {`in lobby: ${lobby.present}`}
     </>
   )
 }
