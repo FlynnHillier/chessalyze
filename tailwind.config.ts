a7a97ff9b25dc5fb96a13915d05edce7ad37e78b
flynnhillier <flynnhillier@gmail.com>
2024-03-10 17:14:57 +0000

    tailwind container queries

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,9 @@
 import type { Config } from "tailwindcss";
+import * as containerQueries from "@tailwindcss/container-queries";
 
 const config: Config = {
-    content: [
-        "./src/**/*.{js,ts,jsx,tsx,mdx}"
-    ],
-    theme: {
-    },
-    plugins: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {},
+  plugins: [containerQueries],
 };
 export default config;
