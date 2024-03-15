4d6efbd6de5fa3594e72905aac3c5db3bb82032b
flynnhillier <flynnhillier@gmail.com>
2024-03-15 16:48:20 +0000

    configured next app to support wss;

--- src/app/_components/providers/trpc.provider.tsx
+++ src/app/_components/providers/trpc.provider.tsx
@@ -1,40 +1,53 @@
-"use client"
+"use client";
 
-import React, { useState } from "react"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { httpBatchLink } from "@trpc/client"
-import superjson from "superjson"
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createWSClient, httpBatchLink, splitLink, wsLink } from "@trpc/client";
+import superjson from "superjson";
 
-import { trpc } from "~/app/_trpc/client"
+import { env } from "~/env";
+import { trpc } from "~/app/_trpc/client";
 
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-}
+const getServerUrl = () => {
+  if (typeof window !== "undefined") return "/api/trpc"; // browser should use relative url
+  return `http://localhost:${env.PORT}/api/trpc`; // dev SSR should use localhost
+};
+
+const getWSSUrl = () => {
+  //TODO: when in production change this from localhost to relative.
+  return `ws://localhost:${env.WSS_PORT}/api/trpc`;
+};
 
 interface ProviderProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export function TRPCProvider({ children }: ProviderProps) {
-  const [queryClient] = useState(() => new QueryClient({}))
+  const [queryClient] = useState(() => new QueryClient({}));
 
   const [trpcClient] = useState(() =>
     trpc.createClient({
       transformer: superjson,
       links: [
-        httpBatchLink({
-          // import 'url' from a server file.
-          url: `${getBaseUrl()}/api/trpc`,
+        splitLink({
+          condition(op) {
+            return op.type === "subscription";
+          },
+          true: wsLink({
+            client: createWSClient({
+              url: getWSSUrl(),
+            }),
+          }),
+          false: httpBatchLink({
+            url: getServerUrl(),
+          }),
         }),
       ],
-    })
-  )
+    }),
+  );
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </trpc.Provider>
-  )
+  );
 }
