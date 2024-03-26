de437952ceea6a1ab81724c60d2f800a05ffe924
flynn hillier <flynnhillier@gmail.com>
2024-03-26 14:48:37 +0000

    remove next-auth dependency & remaining references

--- src/app/(dynamic)/auth/(nonuser)/login/page.tsx
+++ src/app/(dynamic)/auth/(nonuser)/login/page.tsx
@@ -1,20 +1,5 @@
-import { getProviders } from "next-auth/react";
 import SignInBtn from "~/app/_components/auth/signin.btn";
 
 export default async function SignIn() {
-  const providers = await getProviders()
-
-  return (
-    <>
-      {Object.values(providers ?? {}).map((provider) => {
-        return (
-          <div key={provider.name}>
-            <SignInBtn provider={provider} />
-          </div>
-        )
-      })}
-    </>
-  )
-
+  return <></>;
 }
-
--- src/app/(dynamic)/layout.tsx
+++ src/app/(dynamic)/layout.tsx
@@ -1,7 +1,4 @@
-import { getServerSession } from "next-auth";
 import { ReactNode } from "react";
-import { authOptions } from "~/server/auth";
-import SessionProvider from "~/app/_components/providers/session.provider"
 import { TRPCProvider } from "~/app/_components/providers/trpc.provider";
 
 /**
@@ -9,13 +6,9 @@ import { TRPCProvider } from "~/app/_components/providers/trpc.provider";
  * This allows us to cache static pages by only applying session cookies to dynamic pages.
  */
 export default async function Layout({ children }: { children: ReactNode }) {
-  const session = await getServerSession(authOptions)
-
   return (
     <TRPCProvider>
-      <SessionProvider session={session}>
-        <div>{children}</div>
-      </SessionProvider>
+      <div>{children}</div>
     </TRPCProvider>
-  )
+  );
 }
