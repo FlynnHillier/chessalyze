d80f45889f4cd3bf20cefbed20d6ea9e1a2a926c
flynn hillier <flynnhillier@gmail.com>
2024-03-26 14:43:02 +0000

    remove next auth references

--- src/app/_components/providers/session.provider.tsx
+++ src/app/_components/providers/session.provider.tsx
@@ -1,3 +1,4 @@
-"use client"
-import { SessionProvider } from "next-auth/react"
-export default SessionProvider
+"use client";
+export default function SessionProvider() {
+  return <></>;
+}
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,9 +1,2 @@
 //TODO: migrate this to use lucia instead of next auth.
 import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
-
-/**
- * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
- */
-export const getServerAuthSession = () => {
-  //getServerSession(authOptions);
-};
