83a6e3607aa80ab089a924c2463e03d9a5c3a864
flynn hillier <flynnhillier@gmail.com>
2024-03-28 19:06:50 +0000

    added custom colours to dev log

--- src/lib/logging/dev.logger.ts
+++ src/lib/logging/dev.logger.ts
@@ -1,9 +1,33 @@
 import { env } from "~/env";
 
+export enum loggingColourCode {
+  FgBlack = "\x1b[30m",
+  FgRed = "\x1b[31m",
+  FgGreen = "\x1b[32m",
+  FgYellow = "\x1b[33m",
+  FgBlue = "\x1b[34m",
+  FgMagenta = "\x1b[35m",
+  FgCyan = "\x1b[36m",
+  FgWhite = "\x1b[37m",
+  FgGray = "\x1b[90m",
+}
+
 /**
  * Logs specified message only if NODE_ENV enviroment variable is set to 'development'
  * @param message message to log
  */
-export function logDev(...message: any[]) {
-  if (env.NODE_ENV === "development") console.log(...message);
+export function logDev({
+  message,
+  color,
+}: {
+  message: any | any[];
+  color?: `${loggingColourCode}`;
+}) {
+  if (!color) {
+    color = loggingColourCode.FgWhite;
+  }
+
+  if (!Array.isArray(message)) message = [message];
+
+  if (env.NODE_ENV === "development") console.log(color, ...message);
 }
