6d7f07f9d1f0107e0525679588cbbb9744f2b4d9
flynn hillier <flynnhillier@gmail.com>
2024-04-07 21:09:11 +0100

    moves display scroll

--- src/app/(dynamic)/(protected)/play/(panel)/live/page.tsx
+++ src/app/(dynamic)/(protected)/play/(panel)/live/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import Panel from "~/app/(dynamic)/(protected)/play/(panel)/_components/Panel";
 import { useGame } from "~/app/_components/providers/game.provider";
 import { VerboseMovement } from "~/types/game.types";
@@ -11,6 +11,13 @@ import {
 
 export default function LivePanel() {
   const game = useGame();
+
+  const movesScrollEndRef = useRef<null | HTMLSpanElement>(null);
+
+  useEffect(() => {
+    //When a new move happens bring it into view.
+    movesScrollEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [game.game?.moves]);
 
   /**
    * Paired move history, [white move, black move][]
@@ -35,46 +42,47 @@ export default function LivePanel() {
 
   return (
     <Panel>
-      <div className="flex flex-col"></div>
-
-      <div className="flex min-h-40 flex-col rounded-sm bg-stone-950 p-3 [&>div:nth-child(odd)]:bg-stone-900">
-        {pairedMoveHistory.map(([w, b], i) => {
-          return (
-            <div className="flex w-full flex-row items-center bg-stone-800 px-1 py-0.5 first:rounded-t-sm last:rounded-b-sm">
-              <span className="font-medium">{i + 1}.</span>
-              <div className="flex w-1/4 flex-row items-center justify-center gap-0.5 font-bold ">
-                {WhitePieceIcon(w.move.piece)} {w.move.target}
-              </div>
-              <div className="flex w-1/4 flex-row items-center justify-center gap-0.5 font-bold ">
-                {b && BlackPieceIcon(b.move.piece)} {b && b.move.target}
-              </div>
-              <div className="ml-auto grid h-fit flex-grow grid-cols-[1fr_auto] grid-rows-2 gap-x-1">
-                <div className="col-span-2 row-span-1  grid grid-cols-subgrid">
-                  <div className="col-span-1 flex items-center justify-end">
-                    <div
-                      className="block h-1/2 rounded-sm bg-gray-300"
-                      style={{
-                        width: `${100 / (60 / 3.8)}%`,
-                      }}
-                    />
-                  </div>
-                  <div className="col-span-1 col-start-2 text-xs">3.8</div>
+      <div className=" flex h-52 flex-col rounded-sm bg-stone-950 p-3 ">
+        <div className="scrollbar-hide h-full w-full overflow-scroll rounded-sm [&>div:nth-child(odd)]:bg-stone-900">
+          {pairedMoveHistory.map(([w, b], i) => {
+            return (
+              <div className="flex w-full flex-row items-center bg-stone-800 px-1 py-0.5 first:rounded-t-sm last:rounded-b-sm">
+                <span className="font-medium">{i + 1}.</span>
+                <div className="flex w-1/4 flex-row items-center justify-center gap-0.5 font-bold ">
+                  {WhitePieceIcon(w.move.piece)} {w.move.target}
                 </div>
-                <div className="col-span-2 row-span-1  grid grid-cols-subgrid">
-                  <div className="col-span-1 flex items-center justify-end">
-                    <div
-                      className="block h-1/2 rounded-sm bg-zinc-700"
-                      style={{
-                        width: `${100 / (60 / 2.1)}%`,
-                      }}
-                    />
+                <div className="flex w-1/4 flex-row items-center justify-center gap-0.5 font-bold ">
+                  {b && BlackPieceIcon(b.move.piece)} {b && b.move.target}
+                </div>
+                <div className="ml-auto grid h-fit flex-grow grid-cols-[1fr_auto] grid-rows-2 gap-x-1">
+                  <div className="col-span-2 row-span-1  grid grid-cols-subgrid">
+                    <div className="col-span-1 flex items-center justify-end">
+                      <div
+                        className="block h-1/2 rounded-sm bg-gray-300"
+                        style={{
+                          width: `${100 / (60 / 3.8)}%`,
+                        }}
+                      />
+                    </div>
+                    <div className="col-span-1 col-start-2 text-xs">3.8</div>
                   </div>
-                  <div className="col-span-1 col-start-2 text-xs">2.1</div>
+                  <div className="col-span-2 row-span-1  grid grid-cols-subgrid">
+                    <div className="col-span-1 flex items-center justify-end">
+                      <div
+                        className="block h-1/2 rounded-sm bg-zinc-700"
+                        style={{
+                          width: `${100 / (60 / 2.1)}%`,
+                        }}
+                      />
+                    </div>
+                    <div className="col-span-1 col-start-2 text-xs">2.1</div>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+          <span ref={movesScrollEndRef} />
+        </div>
       </div>
     </Panel>
   );
