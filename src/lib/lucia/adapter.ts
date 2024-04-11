ec72cffc36d86a3fab7cad0024f62961055c4dd8
flynn hillier <flynnhillier@gmail.com>
2024-04-11 15:21:59 +0100

    fix import

--- src/lib/lucia/adapter.ts
+++ src/lib/lucia/adapter.ts
@@ -1,5 +1,5 @@
 import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 import { db } from "~/lib/drizzle/db";
-import { users, sessions } from "~/lib/drizzle/schema";
+import { users, sessions } from "~/lib/drizzle/auth.schema";
 
 export const adapter = new DrizzlePostgreSQLAdapter(db, sessions, users);
