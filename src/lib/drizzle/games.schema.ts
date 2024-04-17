37a8af127b373b839ebf0667e2a71ea0c9d3369f
flynnhillier <flynnhillier@gmail.com>
2024-04-17 14:23:11 +0100

    game pgEnums. integrate relation between moves and games table.

--- src/lib/drizzle/games.schema.ts
+++ src/lib/drizzle/games.schema.ts
@@ -6,17 +6,29 @@ import {
   integer,
   primaryKey,
   bigint,
+  pgEnum,
+  unique,
 } from "drizzle-orm/pg-core";
 
 import { users } from "@lib/drizzle/auth.schema";
+import { PROMOTIONPIECE, TILEIDS } from "~/constants/game";
+
+// ENUMS
+export const drizzleEnumTile = pgEnum("tile", TILEIDS);
+export const drizzleEnumPromotionPiece = pgEnum(
+  "promotion_piece",
+  PROMOTIONPIECE,
+);
+
+// TABLES
 
 export const games = pgTable("games", {
-  p_white: varchar("p_white")
-    .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
-  p_black: varchar("p_black")
-    .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
+  p_white: varchar("p_white").references(() => users.id, {
+    onDelete: "set null",
+  }),
+  p_black: varchar("p_black").references(() => users.id, {
+    onDelete: "set null",
+  }),
   id: varchar("id").notNull().primaryKey(),
 });
 
@@ -26,16 +38,16 @@ export const moves = pgTable(
     gameID: varchar("game_id")
       .notNull()
       .references(() => games.id, { onDelete: "cascade" }),
-    turn: integer("turn").notNull().unique(),
-    source: text("source").notNull(),
-    target: text("target").notNull(),
-    t_duration: timestamp("t_duration", { withTimezone: false }),
-    t_start: timestamp("t_start", { withTimezone: true }),
-    t_end: timestamp("t_start", { withTimezone: true }),
+    turn: integer("turn").notNull(),
+    source: drizzleEnumTile("source").notNull(),
+    target: drizzleEnumTile("target").notNull(),
+    promotion: drizzleEnumPromotionPiece("promotion"),
+    t_duration: bigint("t_duration", { mode: "bigint" }),
+    t_start: bigint("t_start", { mode: "bigint" }),
+    t_end: bigint("t_end", { mode: "bigint" }),
   },
-  (table) => {
-    return {
-      pk: primaryKey({ columns: [table.gameID, table.turn] }),
-    };
-  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.gameID, t.turn] }),
+    unq: unique().on(t.gameID, t.turn),
+  }),
 );
