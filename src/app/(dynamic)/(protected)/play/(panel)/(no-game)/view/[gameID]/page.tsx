686d26834468437e824a3e34290f00570804e13d
flynnhillier <flynnhillier@gmail.com>
2024-05-06 13:10:33 +0100

    SSR game context component; view retrospective games;

---
+++ src/app/(dynamic)/(protected)/play/(panel)/(no-game)/view/[gameID]/page.tsx
@@ -0,0 +1,32 @@
+import DispatchServerGameContext from "~/app/_components/controllers/client/DispatchGameContext";
+import Panel from "../../../_components/Panel";
+import TraverseGameMovements from "../../../_components/interfaces/TraverseGameMovements";
+import { redirect } from "next/navigation";
+import { getGameSummary } from "~/lib/drizzle/transactions/game.drizzle";
+
+export default async function ViewRetrospectiveGamePage({
+  params,
+}: {
+  params: { gameID: string };
+}) {
+  const GAME = await getGameSummary(params.gameID);
+
+  if (!GAME) return redirect("/play/view");
+
+  return (
+    <DispatchServerGameContext
+      serverGame={{
+        id: GAME.id,
+        moves: GAME.moves,
+        players: GAME.players,
+        time: {
+          start: GAME.time.start,
+        },
+      }}
+    >
+      <Panel goBackTo="/play/view">
+        <TraverseGameMovements />
+      </Panel>
+    </DispatchServerGameContext>
+  );
+}
---
+++ src/app/_components/controllers/client/DispatchGameContext.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+import {
+  SERVERGAMECONTEXT,
+  useDispatchGame,
+} from "../../providers/client/game.provider";
+
+//TODO: can minimise / refine type of serverGame (not as much info as is current is required.)
+
+/**
+ * A client component which dispatches server game context passed as prop
+ *
+ */
+export default function DispatchServerGameContext({
+  children,
+  serverGame,
+}: {
+  children?: ReactNode;
+  serverGame: NonNullable<SERVERGAMECONTEXT["game"]>;
+}) {
+  const dispatchGame = useDispatchGame();
+
+  useEffect(() => {
+    dispatchGame({
+      type: "LOAD",
+      payload: {
+        id: serverGame.id,
+        moves: serverGame.moves,
+        players: serverGame.players,
+        time: serverGame.time,
+        live: serverGame.live && {
+          FEN: serverGame.live.current.fen,
+          time: {
+            now: Date.now(),
+            remaining: serverGame.live.time.remaining,
+          },
+        },
+      },
+    });
+  }, [serverGame]);
+
+  return <>{children}</>;
+}
