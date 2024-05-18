36cfe887fd933e2f601ce7363d5ce37fe47afb79
flynnhillier <flynnhillier@gmail.com>
2024-05-18 22:23:43 +0100

    update game zod validators to not using union literal mapping

--- src/lib/zod/game/game.validators.ts
+++ src/lib/zod/game/game.validators.ts
@@ -1,4 +1,5 @@
 import { z, ZodType } from "zod";
+import { BW } from "~/types/game.types";
 import {
   CAPTURABLEPIECE,
   DECISIVE_TERMINATIONS,
@@ -17,39 +18,149 @@ export const zUUID = z.string();
 
 export const zFEN = z.string();
 
-export const zDecisiveGameTermination = zStringLiteralUnionFromArray(
-  DECISIVE_TERMINATIONS,
-);
-
-export const zIndecisiveGameTermination =
-  zStringLiteralUnionFromArray(DRAW_TERMINATIONS);
-
-export const zGenericGameTermination = zStringLiteralUnionFromArray([
-  ...DECISIVE_TERMINATIONS,
-  ...DRAW_TERMINATIONS,
+export const zDecisiveGameTermination = z.union([
+  z.literal("checkmate"),
+  z.literal("timeout"),
+  z.literal("resignation"),
 ]);
 
-export const zPromotablePieceSymbol =
-  zStringLiteralUnionFromArray(PROMOTIONPIECE);
+export const zIndecisiveGameTermination = z.union([
+  z.literal("3-fold repitition"),
+  z.literal("50 move rule"),
+  z.literal("insufficient material"),
+  z.literal("stalemate"),
+  z.literal("timeout vs insufficient material"),
+]);
 
-export const zCapturablePieceSymbol =
-  zStringLiteralUnionFromArray(CAPTURABLEPIECE);
+export const zGenericGameTermination = z.union([
+  z.literal("checkmate"),
+  z.literal("timeout"),
+  z.literal("resignation"),
+  z.literal("3-fold repitition"),
+  z.literal("50 move rule"),
+  z.literal("insufficient material"),
+  z.literal("stalemate"),
+  z.literal("timeout vs insufficient material"),
+]);
 
-export const zGenericPieceSymbol = zStringLiteralUnionFromArray(PIECE);
+export const zPromotablePieceSymbol = z.union([
+  z.literal("r"),
+  z.literal("n"),
+  z.literal("b"),
+  z.literal("q"),
+]);
 
-export const zGameTimePreset = zStringLiteralUnionFromArray(TIME_PRESET);
+export const zCapturablePieceSymbol = z.union([
+  z.literal("p"),
+  z.literal("r"),
+  z.literal("n"),
+  z.literal("b"),
+  z.literal("q"),
+]);
 
-export const zSquare = zStringLiteralUnionFromArray(TILEIDS);
+export const zGenericPieceSymbol = z.union([
+  z.literal("p"),
+  z.literal("r"),
+  z.literal("n"),
+  z.literal("b"),
+  z.literal("q"),
+  z.literal("k"),
+]);
+
+export const zGameTimePreset = z.union([
+  z.literal("30s"),
+  z.literal("1m"),
+  z.literal("5m"),
+  z.literal("10m"),
+  z.literal("15m"),
+  z.literal("30m"),
+  z.literal("1h"),
+]);
+
+export const zSquare = z.union([
+  z.literal("a1"),
+  z.literal("a2"),
+  z.literal("a3"),
+  z.literal("a4"),
+  z.literal("a5"),
+  z.literal("a6"),
+  z.literal("a7"),
+  z.literal("a8"),
+  z.literal("b1"),
+  z.literal("b2"),
+  z.literal("b3"),
+  z.literal("b4"),
+  z.literal("b5"),
+  z.literal("b6"),
+  z.literal("b7"),
+  z.literal("b8"),
+  z.literal("c1"),
+  z.literal("c2"),
+  z.literal("c3"),
+  z.literal("c4"),
+  z.literal("c5"),
+  z.literal("c6"),
+  z.literal("c7"),
+  z.literal("c8"),
+  z.literal("d1"),
+  z.literal("d2"),
+  z.literal("d3"),
+  z.literal("d4"),
+  z.literal("d5"),
+  z.literal("d6"),
+  z.literal("d7"),
+  z.literal("d8"),
+  z.literal("e1"),
+  z.literal("e2"),
+  z.literal("e3"),
+  z.literal("e4"),
+  z.literal("e5"),
+  z.literal("e6"),
+  z.literal("e7"),
+  z.literal("e8"),
+  z.literal("f1"),
+  z.literal("f2"),
+  z.literal("f3"),
+  z.literal("f4"),
+  z.literal("f5"),
+  z.literal("f6"),
+  z.literal("f7"),
+  z.literal("f8"),
+  z.literal("g1"),
+  z.literal("g2"),
+  z.literal("g3"),
+  z.literal("g4"),
+  z.literal("g5"),
+  z.literal("g6"),
+  z.literal("g7"),
+  z.literal("g8"),
+  z.literal("h1"),
+  z.literal("h2"),
+  z.literal("h3"),
+  z.literal("h4"),
+  z.literal("h5"),
+  z.literal("h6"),
+  z.literal("h7"),
+  z.literal("h8"),
+]);
 
 export const zColor = z.union([z.literal("w"), z.literal("b")]);
 
-export const zBW = (type: ZodType) =>
-  z.object({
+export function zBW<Z extends ZodType>(type: Z) {
+  return z.object({
     w: type,
     b: type,
   });
-
-export const zCapturedPiecesMapping = zBW(zRecord(CAPTURABLEPIECE, z.number()));
+}
+export const zCapturedPiecesMapping = zBW(
+  z.object({
+    p: z.number(),
+    r: z.number(),
+    q: z.number(),
+    n: z.number(),
+    b: z.number(),
+  }),
+);
 
 export const zPlayer = z.object({
   pid: z.string(),
@@ -70,7 +181,7 @@ export const zVerboseMovement = z.object({
   time: z.object({
     sinceStart: z.number(),
     timestamp: z.number(),
-    remaining: zBW(z.number()).optional(),
+    remaining: zBW(z.number()).optional(), //z.object({ w: z.number(), b: z.number() }).optional(),
     moveDuration: z.number(),
   }),
   initiator: z.object({
@@ -88,9 +199,19 @@ export const zGameSnapshot = z.object({
   time: z.object({
     start: z.number(),
     now: z.number(),
-    remaining: zBW(z.number()).optional(),
+    remaining: z
+      .object({
+        w: z.number(),
+        b: z.number(),
+      })
+      .optional(),
     initial: z.object({
-      remaining: zBW(z.number()).optional(),
+      remaining: z
+        .object({
+          w: z.number(),
+          b: z.number(),
+        })
+        .optional(),
     }),
   }),
   moves: z.array(zVerboseMovement),
