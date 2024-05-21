0868c5db8dc44df3d264467f1035d1d553075590
flynnhillier <flynnhillier@gmail.com>
2024-05-21 13:29:02 +0100

    SummaryInfiniteScroll update in real-time using ws

--- src/app/_components/game/summary/SummaryInfiniteScroll.tsx
+++ src/app/_components/game/summary/SummaryInfiniteScroll.tsx
@@ -1,9 +1,12 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import InfiniteScroller from "../../common/scroll/InfiniteScroll";
 import { trpc } from "~/app/_trpc/client";
 import { GameSummary } from "~/types/game.types";
 import { GameSummaryPill } from "./GameSummaryPill";
 import SyncLoader from "../../loading/SyncLoader";
+import { useWebSocket } from "next-ws/client";
+import { wsServerToClientMessage } from "~/lib/ws/messages/client.messages.ws";
+import { wsClientToServerMessage } from "~/lib/ws/messages/server.messages.ws";
 
 /**
  * Infinite scroll over recent game summarys.
@@ -12,14 +15,49 @@ import SyncLoader from "../../loading/SyncLoader";
 export default function SummaryInfiniteScroll({
   countPerScroll = 5,
 }: {
-  countPerScroll: number;
+  countPerScroll?: number;
 }) {
   const [isMore, setIsMore] = useState<boolean>(true);
   const tail = useRef<number>(0);
   const [summarys, setSummarys] = useState<GameSummary[]>([]);
 
+  const ws = useWebSocket();
   const trpcSummaryInfiniteScroll =
     trpc.game.summary.infiniteScroll.useMutation();
+
+  /**
+   * Emit subscribe event to server so that component receives message when new game summary occurs
+   */
+  useEffect(() => {
+    if (ws?.readyState === 1)
+      ws.send(
+        wsClientToServerMessage.send("SUMMARY_SUBSCRIBE").data({}).stringify(),
+      );
+  }, [ws?.readyState]);
+
+  /**
+   * Add handlers for incoming ws message, such that summarys are updated when data is received.
+   */
+  useEffect(() => {
+    if (!ws) return;
+
+    function onMessage(message: MessageEvent) {
+      return wsServerToClientMessage.receiver({
+        SUMMARY_NEW: (summary) => {
+          setSummarys((p) => [summary, ...p]);
+          tail.current++;
+        },
+      })(message.data);
+    }
+
+    ws.addEventListener("message", onMessage);
+
+    return () => {
+      ws.removeEventListener("message", onMessage);
+
+      //TODO: add unsubscribe emit here
+    };
+  }, [ws]);
 
   async function fetchNext() {
     const res = await trpcSummaryInfiniteScroll.mutateAsync({
@@ -48,7 +86,7 @@ export default function SummaryInfiniteScroll({
         }
       >
         {summarys.map((summary, i) => (
-          <GameSummaryPill summary={summary} key={i} redirect={true} />
+          <GameSummaryPill summary={summary} key={summary.id} redirect={true} />
         ))}
       </InfiniteScroller>
     </div>
