bebba2a239fee04f75184a199a7575fa19cf170b
flynnhillier <flynnhillier@gmail.com>
2024-05-22 14:42:39 +0100

    /play/view panel

--- src/app/(dynamic)/(protected)/play/(panel)/(no-game)/view/page.tsx
+++ src/app/(dynamic)/(protected)/play/(panel)/(no-game)/view/page.tsx
@@ -1,3 +1,12 @@
-export default function ViewRetrospectiveGame() {
-  return <></>;
+import Panel from "../../_components/Panel";
+import SummaryInfiniteScroll from "~/app/_components/game/summary/SummaryInfiniteScroll";
+
+export default async function ViewRetrospectiveGame() {
+  return (
+    <Panel subtitle="view game" goBackTo="/play">
+      <div className="h-96">
+        <SummaryInfiniteScroll />
+      </div>
+    </Panel>
+  );
 }
