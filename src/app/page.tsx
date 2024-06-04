ffcf1e869e0597cff59bae1e34021acdc2cf245c
flynnhillier <flynnhillier@gmail.com>
2024-06-04 18:58:28 +0100

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-"use server";
-
-import { GameSummarysScroller } from "./_components/game/summary/GameSummaryPill";
-import { serverClient } from "~/app/_trpc/serverClient";
-
 export default async function Home() {
-  return (
-    <GameSummarysScroller summarys={await serverClient.game.summary.recent()} />
-  );
+  return <>home</>;
 }
