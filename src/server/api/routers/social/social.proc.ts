25627850ea71ae78c62436ada5938a36b537f107
flynnhillier <flynnhillier@gmail.com>
2024-06-24 15:57:43 +0100

    trpc routes for social

--- src/server/api/routers/social/social.proc.ts
+++ src/server/api/routers/social/social.proc.ts
@@ -1,8 +1,79 @@
-import { trpcSocialSendFriendRequestProcedure } from "~/server/api/routers/social/procedures/social.FriendRequest.proc";
+import z from "zod";
+import { TRPCError } from "@trpc/server";
+
+import { SOCIALPROCEDURE } from "~/server/api/routers/social/social.trpc";
 import { createTRPCRouter } from "~/server/api/trpc";
+import DrizzleSocialTransaction from "~/lib/drizzle/transactions/social.transactions.drizzle";
 
 export const trpcSocialRouter = createTRPCRouter({
-  friendRequest: createTRPCRouter({
-    send: trpcSocialSendFriendRequestProcedure,
+  friend: createTRPCRouter({
+    request: createTRPCRouter({
+      send: SOCIALPROCEDURE.input(
+        z.object({ targetUserID: z.string() }),
+      ).mutation(async ({ input, ctx }) => {
+        if (input.targetUserID === ctx.user.id)
+          throw new TRPCError({
+            message: "cannot send friend request to self",
+            code: "FORBIDDEN",
+          });
+
+        const { success, meta } = await new DrizzleSocialTransaction(
+          ctx.user.id,
+        ).sendUserFriendRequest(input.targetUserID);
+
+        return {
+          success,
+          message:
+            !success && meta?.isNonExistentTargetUserError
+              ? "user not found"
+              : undefined,
+        };
+      }),
+      accept: SOCIALPROCEDURE.input(
+        z.object({ targetUserID: z.string() }),
+      ).mutation(async ({ input, ctx }) => {
+        const { success, meta } = await new DrizzleSocialTransaction(
+          ctx.user.id,
+        ).acceptUserFriendRequest(input.targetUserID);
+
+        return {
+          success,
+          message:
+            !success && meta?.isNonExistentTargetUserError
+              ? "user not found"
+              : undefined,
+        };
+      }),
+      rejectIncoming: SOCIALPROCEDURE.input(
+        z.object({ targetUserID: z.string() }),
+      ).mutation(async ({ input, ctx }) => {
+        const { success, meta } = await new DrizzleSocialTransaction(
+          ctx.user.id,
+        ).cancelIncomingFriendRequest(input.targetUserID);
+
+        return {
+          success,
+          message:
+            !success && meta?.isNonExistentTargetUserError
+              ? "user not found"
+              : undefined,
+        };
+      }),
+      cancelOutgoing: SOCIALPROCEDURE.input(
+        z.object({ targetUserID: z.string() }),
+      ).mutation(async ({ input, ctx }) => {
+        const { success, meta } = await new DrizzleSocialTransaction(
+          ctx.user.id,
+        ).cancelOutgoingFriendRequest(input.targetUserID);
+
+        return {
+          success,
+          message:
+            !success && meta?.isNonExistentTargetUserError
+              ? "user not found"
+              : undefined,
+        };
+      }),
+    }),
   }),
 });
